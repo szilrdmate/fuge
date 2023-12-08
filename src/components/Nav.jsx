44215d22e2bce2ca443eb2385e73f3df6bf38d99
Szilárd Máté <mate.szilard.mark@gmail.com>
2023-12-09 00:19:16 +0100

    Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,74 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-function Nav() {
+const Header = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <>
-      <nav>
-        <div className="navbar">
-          <Link to="/">
-            <img src="" alt="Füge logo" />
-          </Link>
-          <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#blog">Blog</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
-}
+    <header>
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo on the left */}
+        <Link to="/" className="logo-link">
+          <img src="/assets/logo_white.svg" alt="Füge logo" />
+        </Link>
 
-export default Nav;
+        {/* Responsive Navigation Menu on the right */}
+        <ul className="nav-links">
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/about">ABOUT</NavLink>
+          <NavLink to="/blog">BLOG</NavLink>
+          <NavLink to="/contact">CONTACT</NavLink>
+        </ul>
+
+        {/* Hamburger Menu for small screens */}
+        <div className="relative">
+          <button className="mobile-menu-btn" onClick={toggleMobileMenu}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
+
+          {/* Mobile Menu Dropdown */}
+          {isMobileMenuOpen && (
+            <div className="mobile-menu-dropdown">
+              <DropdownLink to="/" onClick={toggleMobileMenu}>
+                HOME
+              </DropdownLink>
+              <DropdownLink to="/about" onClick={toggleMobileMenu}>
+                ABOUT
+              </DropdownLink>
+              <DropdownLink to="/blog" onClick={toggleMobileMenu}>
+                BLOG
+              </DropdownLink>
+              <DropdownLink to="/contact" onClick={toggleMobileMenu}>
+                CONTACT
+              </DropdownLink>
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+// NavLink component for consistent styling
+const NavLink = ({ to, children }) => (
+  <li>
+    <Link to={to} className="nav-link">
+      {children}
+    </Link>
+  </li>
+);
+
+// DropdownLink for mobile menu items
+const DropdownLink = ({ to, children, onClick }) => (
+  <Link to={to} onClick={onClick} className="dropdown-link">
+    {children}
+  </Link>
+);
+
+export default Header;
