7187ba5a7eb2a75ffb2c43f1eea9f67d69414e47
Szilárd Máté <mate.szilard.mark@gmail.com>
2023-12-16 12:10:09 +0100

    Hero section structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,34 @@ const Hero = () => {
     const [t, i18n] = useTranslation("global")
 
     return (
-        <div className="hero-wrapper">
-            <h1>{t("home.body")}</h1>
-        </div>
+        <>
+            <div className="hero-wrapper">
+                <h1>{t("home.body")}</h1>
+            </div>
+            <div className="grid-wrapper">
+                <div className="nested-grid">
+                    <div className="grid-item">
+                        <h1>OUR STORY</h1>
+                    </div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                </div>
+                <div className="nested-grid">
+                    <div className="grid-item">
+                        <h1>OUR NEWS</h1>
+                    </div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                </div>
+                <div className="nested-grid">
+                    <div className="grid-item">
+                        <h1>OUR TEAM</h1>
+                    </div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                </div>
+            </div>
+        </>
     )
 }
 
