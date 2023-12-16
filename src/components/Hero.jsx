46418ad292ba6c46fd922f392d87278d9dcbb44a
Szilárd Máté <mate.szilard.mark@gmail.com>
2023-12-16 15:26:58 +0100

    Grid layout, scss

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,30 +7,53 @@ const Hero = () => {
     return (
         <>
             <div className="hero-wrapper">
-                <h1>{t("home.body")}</h1>
+                
             </div>
+
             <div className="grid-wrapper">
                 <div className="nested-grid">
                     <div className="grid-item">
                         <h1>OUR STORY</h1>
                     </div>
-                    <div className="grid-item"></div>
-                    <div className="grid-item"></div>
+                    <div className="grid-item">
+                        <h1>MKB</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, est quas veniam consequatur iusto aspernatur hic amet minus repellat soluta minima facere </p>
+                    </div>
+                    <div className="grid-item">
+                        <h1>MKB</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, est quas veniam consequatur iusto aspernatur hic amet minus repellat soluta minima facere </p>
+                    </div>
                 </div>
                 <div className="nested-grid">
                     <div className="grid-item">
-                        <h1>OUR NEWS</h1>
+                        <h1>{t("home.body")}</h1>
                     </div>
-                    <div className="grid-item"></div>
-                    <div className="grid-item"></div>
+                    <div className="grid-item">
+                        <h1>MKB</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, est quas veniam consequatur iusto aspernatur hic amet minus repellat soluta minima facere </p>
+                    </div>
+                    <div className="grid-item">
+                        <h1>MKB</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, est quas veniam consequatur iusto aspernatur hic amet minus repellat soluta minima facere </p>
+                    </div>
                 </div>
                 <div className="nested-grid">
                     <div className="grid-item">
                         <h1>OUR TEAM</h1>
                     </div>
-                    <div className="grid-item"></div>
-                    <div className="grid-item"></div>
+                    <div className="grid-item">
+                        <h1>MKB</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, est quas veniam consequatur iusto aspernatur hic amet minus repellat soluta minima facere </p>
+                    </div>
+                    <div className="grid-item">
+                        <h1>MKB</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, est quas veniam consequatur iusto aspernatur hic amet minus repellat soluta minima facere </p>
+                    </div>
                 </div>
+            </div>
+
+            <div id="social" className="social-wrapper">
+
             </div>
         </>
     )
